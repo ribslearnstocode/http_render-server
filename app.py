e28193cda269262bd95f73fe58c5ffcbdb9a2ac3
unknown <ribence.kadel456@gmail.com>
2022-12-02 13:19:45 +0545

    ijsoifjf

--- app.py
+++ app.py
@@ -1,57 +1,19 @@
-# import asyncio
-# import signal
-# import os
-
-# import websockets
-
-
-# async def echo(websocket):
-#     async for message in websocket:
-#         await websocket.send(message)
-
-
-# async def main():
-#     # Set the stop condition when receiving SIGTERM.
-#     loop = asyncio.get_running_loop()
-#     stop = loop.create_future()
-#     loop.add_signal_handler(signal.SIGTERM, stop.set_result, None)
-
-#     async with websockets.serve(
-#         echo,
-#         host="",
-#         port=int(os.environ["PORT"]),
-#     ):
-#         await stop
-
-
-# if __name__ == "__main__":
-#     asyncio.run(main())
-
 import asyncio
-import signal
-import json
-import websockets
-from all_vuln_one_var import run_program
-import os
 import http
+import signal
 
-async def hello():  # put application's code here
-    await run_program()
-    return json({'about': "Hello world"})
+import websockets
+
 
 async def echo(websocket):
     async for message in websocket:
-        print(message)
-        message=message.split(" ")
-        if message[0] == "start":
-            final_message = run_program(message[1])
-            print("jjj",final_message)
+        await websocket.send(message)
 
-        await websocket.send(json.dumps(final_message))
 
 async def health_check(path, request_headers):
     if path == "/healthz":
         return http.HTTPStatus.OK, [], b"OK\n"
+
 
 async def main():
     # Set the stop condition when receiving SIGTERM.
@@ -66,6 +28,7 @@ async def main():
         process_request=health_check,
     ):
         await stop
+
 
 if __name__ == "__main__":
     asyncio.run(main())
